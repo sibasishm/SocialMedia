4d7adc040331502cf5182c463d25cb1812344ad2
Sibasish Mohanty <smsibasish@gmail.com>
2020-04-13 20:31:22 +0530

    introducing factory functions for common controllers

---
+++ controllers/factory.js
@@ -0,0 +1,43 @@
+const AppError = require('../utils/appError');
+const { catchAsync } = require('../utils/helper');
+
+exports.deleteOne = (Model) =>
+	catchAsync(async (req, res, next) => {
+		const doc = await Model.findByIdAndDelete(req.params.id);
+
+		if (!doc) {
+			return next(new AppError('Document not found', 404));
+		}
+
+		res.status(204).json({
+			status: 'success',
+			data: null,
+		});
+	});
+
+exports.updateOne = (Model) =>
+	catchAsync(async (req, res, next) => {
+		const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+			runValidators: true,
+		});
+
+		if (!doc) {
+			return next(new AppError('Document not found', 404));
+		}
+
+		res.status(200).json({
+			status: 'success',
+			data: doc,
+		});
+	});
+
+exports.createOne = (Model) =>
+	catchAsync(async (req, res, next) => {
+		const doc = await Model.create(req.body);
+
+		res.status(201).json({
+			status: 'success',
+			data: doc,
+		});
+	});
