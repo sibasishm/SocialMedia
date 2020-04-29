a49894da537083f32b664432ae40654847352edc
Sibasish Mohanty <smsibasish@gmail.com>
2020-04-29 20:57:37 +0530

    group based authorization first attempt, better middleware structure

--- controllers/groups.js
+++ controllers/groups.js
@@ -1,3 +1,4 @@
+var ObjectId = require('mongoose').Types.ObjectId;
 const Group = require('../models/Group');
 const AppError = require('../utils/appError');
 const { catchAsync, select } = require('../utils/helper');
@@ -6,14 +7,45 @@ exports.isGroupFound = catchAsync(async (req, res, next, id) => {
 	const group = await Group.findById(id);
 
 	if (!group) {
-		return next(new AppError('Group not found', 404));
+		return next(new AppError('Group not found.', 404));
 	}
-
+	req.group = group;
 	next();
 });
 
+exports.isPrivateGroup = (req, res, next) => {
+	if (req.group.privateGroup) {
+		return next(
+			new AppError(
+				'This is a private group. You are not allowed to perform this action.',
+				403
+			)
+		);
+	}
+	next();
+};
+
+exports.restrictToGroupRole = (role) => (req, res, next) => {
+	console.log(typeof req.group[role]);
+	if (
+		(Array.isArray(req.group[role]) &&
+			!req.group[role].includes(req.user._id)) ||
+		(ObjectId.isValid(req.group[role]) &&
+			!req.group[role].equals(req.user._id))
+	) {
+		return next(
+			new AppError('You are not allowed to perform this action.', 403)
+		);
+	}
+	next();
+};
+
 exports.createGroup = catchAsync(async (req, res, next) => {
-	const group = await Group.create(req.body);
+	const group = await Group.create({
+		...req.body,
+		admin: req.user._id,
+		members: [req.user._id],
+	});
 
 	res.status(201).json({
 		status: 'success',
@@ -32,7 +64,7 @@ exports.getAllGroups = catchAsync(async (req, res, next) => {
 });
 
 exports.getGroup = catchAsync(async (req, res, next) => {
-	const group = await Group.findById(req.params.id)
+	const group = await Group.findById(req.group._id)
 		.populate({ path: 'members', select })
 		.populate({ path: 'admin', select });
 
@@ -47,11 +79,14 @@ exports.updateGroup = catchAsync(async (req, res, next) => {
 
 	if (members || admin) {
 		return next(
-			new AppError('Please add members using the /join route.', 400)
+			new AppError(
+				'Please add members using the /addMember or /join route.',
+				400
+			)
 		);
 	}
 	const updatedGroup = await Group.findByIdAndUpdate(
-		req.params.id,
+		req.group._id,
 		req.body,
 		{
 			new: true,
@@ -65,19 +100,19 @@ exports.updateGroup = catchAsync(async (req, res, next) => {
 	});
 });
 
-exports.joinGroup = catchAsync(async (req, res, next) => {
-	const group = await Group.findById(req.params.id);
+exports.updateMembers = catchAsync(async (req, res, next) => {
+	const group = req.group;
 
-	if (group.privateGroup || group.members.length === group.maxGroupSize) {
+	if (group.members.length === group.maxGroupSize) {
 		return next(
-			new AppError('You are not allowed to join this group', 403)
+			new AppError("This group has reached it's size limit.", 403)
 		);
 	}
 
 	isAlreadyJoined = group.members.includes(req.user._id);
 	if (isAlreadyJoined) {
 		const updatedGroup = await Group.findByIdAndUpdate(
-			req.params.id,
+			req.group._id,
 			{ $pull: { members: req.user._id } },
 			{ new: true }
 		);
@@ -97,7 +132,7 @@ exports.joinGroup = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteGroup = catchAsync(async (req, res, next) => {
-	await Group.findByIdAndDelete(req.params.id);
+	await Group.findByIdAndDelete(req.group._id);
 
 	res.status(204).json({
 		status: 'success',
--- routes/groups.js
+++ routes/groups.js
@@ -3,27 +3,32 @@ const {
 	createGroup,
 	getAllGroups,
 	isGroupFound,
+	isPrivateGroup,
 	updateGroup,
 	deleteGroup,
 	getGroup,
-	joinGroup,
+	updateMembers,
+	restrictToGroupRole,
 } = require('../controllers/groups');
-const { protect, restrictTo } = require('../controllers/auth');
+const { protect } = require('../controllers/auth');
 
 const router = express.Router();
 
-router
-	.route('/')
-	.post(protect, restrictTo('admin'), createGroup)
-	.get(getAllGroups);
+router.route('/').post(protect, createGroup).get(getAllGroups);
 
 router.param('id', isGroupFound);
 router
 	.route('/:id')
-	.get(protect, getGroup)
-	.patch(protect, restrictTo('admin'), updateGroup)
-	.delete(protect, restrictTo('admin'), deleteGroup);
+	.get(protect, restrictToGroupRole('members'), getGroup)
+	.patch(protect, restrictToGroupRole('admin'), updateGroup)
+	.delete(protect, restrictToGroupRole('admin'), deleteGroup);
 
-router.post('/:id/join', protect, restrictTo('user', 'admin'), joinGroup);
+router.post('/:id/join', protect, isPrivateGroup, updateMembers);
+router.post(
+	'/:id/addMember',
+	protect,
+	restrictToGroupRole('admin'),
+	updateMembers
+);
 
 module.exports = router;
